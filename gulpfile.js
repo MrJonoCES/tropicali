97df071b4f747be5db1552c32fde9f2c2ba06bc5
Jono L <contact@cityeditionstudio.co.uk>
2021-06-08 10:05:08 +0100

    end of video 7

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,17 @@ var sass = require('gulp-sass');
 
 sass.compiler = require('node-sass');
 
-gulp.task('default',function() {
+gulp.task("sass", function(){ 
     // we want to run "sass css/app.scss app.css --watch"
     return gulp.src("css/app.scss")
         .pipe(sass())
-        .pipe(gulp.dest("app.css"))
-});
+        .pipe(gulp.dest("."))
+})
+
+gulp.task ("watch", function(){
+    gulp.watch("css/app.scss", ["sass"])
+
+})
+
+
+gulp.task('default', ["sass", "watch"],);
